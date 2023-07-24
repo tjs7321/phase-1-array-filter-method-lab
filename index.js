50888b52fea50c837002770ec89b7185f722ee6a
Theodore Smith <tjs7321@gmail.com>
2023-07-23 23:42:40 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(array, string) {
+    let newArray = []
+    for (let driver of array) {
+        if (driver.toLowerCase() === string.toLowerCase()) {
+            newArray.push(driver)
+        }
+    }
+    return newArray
+}
+
+function fuzzyMatch(array, string) {
+    let newArray = []
+    for (let driver of array) {
+        if ((driver.toLowerCase()).startsWith(string.toLowerCase())) {
+            newArray.push(driver)
+        }
+    }
+    return newArray
+}
+
+function matchName(array, string) {
+    let newArray = []
+    for (let driver of array) {
+        if (driver.name.toLowerCase() === string.toLowerCase()) {
+            newArray.push(driver)
+        }
+    }
+    return newArray
+}
